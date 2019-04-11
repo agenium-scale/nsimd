492dcea8035b9e46c7ce3d5b8bdd16755d1b4065
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-11 16:15:45 +0200

    Still trying for windows

--- scripts/travis-windows.ps1
+++ scripts/travis-windows.ps1
@@ -1,3 +1,3 @@
 choco install -y ninja
-choco install visualstudio2017-workload-vctools -y
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat x64
