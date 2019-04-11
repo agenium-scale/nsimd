8571cdc0c4fe964a57f11fdcdc63aed1b1c7375a
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-11 16:37:15 +0200

    Still trying for windows

--- scripts/travis-windows.ps1
+++ scripts/travis-windows.ps1
@@ -1,3 +1,2 @@
 choco install -y ninja
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+& "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
