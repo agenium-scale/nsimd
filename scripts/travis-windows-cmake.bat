582a5f49fcf8aa15c66270b1343b4b56f2894585
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-12 10:27:22 +0200

    Debugging windows

---
+++ scripts/travis-windows-cmake.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+cmake .. -DPYTHON_EXECUTABLE=C:\\Python37\\python -GNinja -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" %*
