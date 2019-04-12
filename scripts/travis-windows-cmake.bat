19d80ea6e04bf65143b765478c239a3ff1c8dc79
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-12 16:26:44 +0200

    Debugging windows

--- scripts/travis-windows-cmake.bat
+++ scripts/travis-windows-cmake.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-dir /S "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7/IDE/Microsoft/ADL Tools/"
+dir "C:\Program Files (x86)\Microsoft Visual Studio 14.0\"
 cmake .. -DPYTHON_EXECUTABLE=C:\\Python37\\python -GNinja -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" %*
