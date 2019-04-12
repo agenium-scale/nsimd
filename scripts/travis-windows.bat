92ad9723eeea8a0d619eb41da69cb2eee00580eb
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-12 14:50:27 +0200

    Debugging windows

--- scripts/travis-windows.bat
+++ scripts/travis-windows.bat
@@ -1,4 +1,8 @@
 choco install -y ninja
 choco install -y python3
 choco install -y windows-sdk-10.0
+choco install -y vcredist140
+choco install -y vcredist2015
+choco install -y vcredist2017
+choco install -y vcredist-all
 refreshenv
