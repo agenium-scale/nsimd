20d0822c8e02ffd5388f14fcc34fd58f12b639bf
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-12 15:16:08 +0200

    Debugging windows

--- scripts/travis-windows.bat
+++ scripts/travis-windows.bat
@@ -1,8 +1,4 @@
 choco install -y ninja
 choco install -y python3
-choco install -y windows-sdk-10.0
-choco install -y vcredist140
-choco install -y vcredist2015
-choco install -y vcredist2017
-choco install -y vcredist-all
+choco install -y microsoft-visual-cpp-build-tools --installargs "/InstallSelectableItems 'Win10SDK_VisibleV1'"
 refreshenv
