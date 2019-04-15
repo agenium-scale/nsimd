06831b504bb4905e4e23ce0b5cd755960340b4fd
Charly Chevalier <charlyy.chevalier@gmail.com>
2019-04-15 22:28:01 +0200

    Still testing windows

--- scripts/windows-env.bat
+++ scripts/windows-env.bat
@@ -2,4 +2,4 @@ REM Load MSVC compiler env
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 REM Convert fresh env to bash env
-bash -c "env | sed 's/^/export /' | sed 's/=/=''/' | sed 's/$/''/'" > _env.sh
+bash -c "env | sed 's/^/export /' | sed ""s/=/='/"" | sed ""s/$/'/""" > _env.sh
