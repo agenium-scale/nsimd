90756eed001ffea09ea35c8b1620abc9913f72c2
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-15 17:55:24 +0200

    Testing setx

--- scripts/travis-windows-cmake.bat
+++ scripts/travis-windows-cmake.bat
@@ -17,6 +17,11 @@ echo INCLUDE=
 echo %INCLUDE%
 echo ---
 
+setx INCLUDE=%INCLUDE%
+setx LIB=%LIB%
+setx LIBPATH=%LIBPATH%
+setx PATH=%PATH%
+
 
 REM
 REM set "INCLUDE=%INCLUDE%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\include"
@@ -25,5 +30,5 @@ REM
 REM set "PATH=C:\Python37\python;%PATH%"
 
 REM Now we can call cmake now that cl.exe can be found
-REM cmake -DPYTHON_EXECUTABLE=C:\\Python37\\python %*
+cmake -DPYTHON_EXECUTABLE=C:\\Python37\\python %*
 REM cmake --build .
