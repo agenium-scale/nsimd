f19c98e830706555cbb1bbe5f0b1bb9257a0cc97
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-15 16:28:44 +0200

    Rework travis for windows

--- scripts/travis-windows-cmake.bat
+++ scripts/travis-windows-cmake.bat
@@ -1,15 +1,15 @@
 echo %INCLUDE%
 
 REM Make sure to load MSVC compiler env
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
 echo %INCLUDE%
 
 REM
-set "INCLUDE=%INCLUDE%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\include"
+REM set "INCLUDE=%INCLUDE%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\include"
 
 REM
-set "PATH=C:\Python37\python;%PATH%"
+REM set "PATH=C:\Python37\python;%PATH%"
 
 REM Now we can call cmake now that cl.exe can be found
-cmake %*
+cmake -DPYTHON_EXECUTABLE=C:\\Python37\\python %*
