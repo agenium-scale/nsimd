66e71f571dd8b84083edaa4d2ea9f6575b9a2a7b
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-15 10:31:21 +0200

    Still debugging windows

--- scripts/travis-windows-cmake.bat
+++ scripts/travis-windows-cmake.bat
@@ -1,3 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-dir /S "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft"
+echo "Scanning dirs: BEGIN"
+dir /S "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions"
+echo "Scanning dirs: END"
 cmake .. -DPYTHON_EXECUTABLE=C:\\Python37\\python -GNinja -DCMAKE_C_COMPILER="cl.exe" -DCMAKE_CXX_COMPILER="cl.exe" %*
