7dd7d5601be37ee018635de442115b384f502544
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-15 14:40:34 +0200

    Look for headers...

--- scripts/travis-windows.bat
+++ scripts/travis-windows.bat
@@ -1,3 +1,7 @@
-choco install -y ninja
-choco install -y python3
-choco install -y visualstudio2017buildtools --force --package-parameters "--allWorkloads --includeRecommended --includeOptional --quiet"
+REM choco install -y ninja
+REM choco install -y python3
+REM choco install -y visualstudio2017buildtools --force --package-parameters "--allWorkloads --includeRecommended --includeOptional --quiet"
+cd %HOMEPATH%
+dir /s emmintrin.h
+cd C:\Program Files (x86)
+dir /s emmintrin.h
