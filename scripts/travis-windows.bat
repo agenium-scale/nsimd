9498b5e322480111acc9ae6cf02b3cc2dff548fc
Charly Chevalier <charly.chevalier@numscale.com>
2019-04-25 16:49:59 +0200

    Re-enabled everything for travis + Little renaming

--- scripts/travis-windows.bat
+++
@@ -1,7 +0,0 @@
-REM choco install -y ninja
-REM choco install -y python3
-REM choco install -y visualstudio2017buildtools --force --package-parameters "--allWorkloads --includeRecommended --includeOptional --quiet"
-cd %HOMEPATH%
-dir /s emmintrin.h
-cd C:\Program Files (x86)
-dir /s emmintrin.h
