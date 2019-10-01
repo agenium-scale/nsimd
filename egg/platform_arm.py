115bece7e7b6ac5be732402df02b4f2183f66ed2
Adrien Arnaud <adrien.arnaud@numscale.com>
2019-10-01 16:29:39 +0200

    Updated f16 support for zip in arm

--- egg/platform_arm.py
+++ egg/platform_arm.py
@@ -1690,7 +1690,7 @@ def downcvt1(simd_ext, from_typ, to_typ):
 
 def zip_half(func, simd_ext, typ):
     if simd_ext in ['aarch64', 'sve']:
-        if typ =='f16':
+        if typ =='f16' and simd_ext == 'aarch64':
             return '''\
             #ifdef NSIMD_FP16
             return {s}v{op}{q}_{suf}({in0}, {in1});
