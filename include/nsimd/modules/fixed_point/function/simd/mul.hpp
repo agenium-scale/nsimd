2cfc3bb83900279eeabd05b599395732751a8ef3
Kenny Peou <kenny.peou@numscale.com>
2019-10-24 16:40:06 +0200

    Add decompressed support to fixed point mul

--- include/nsimd/modules/fixed_point/function/simd/mul.hpp
+++ include/nsimd/modules/fixed_point/function/simd/mul.hpp
@@ -30,28 +30,30 @@ SOFTWARE.
 
 namespace nsimd {
 namespace fixed_point {
+
 template <uint8_t _lf, uint8_t _rt>
-NSIMD_INLINE fpsimd_t<_lf, _rt> simd_mul(const fpsimd_t<_lf, _rt> &a,
-                                         const fpsimd_t<_lf, _rt> &b) {
+NSIMD_INLINE fpsimd_uc<_lf,_rt> simd_mul_uc( const fpsimd_uc<_lf,_rt> &a,
+                                             const fpsimd_uc<_lf,_rt> &b ) {
   typedef typename fp_t<_lf, _rt>::value_up up_t;
   typedef typename fp_t<_lf, _rt>::value_type val_t;
-  typedef typename fp_t<_lf, _rt>::simd_up simd_up_t;
   const int n_bits = 8 * sizeof(val_t);
   const int shift = n_bits - _lf;
 
-  simd_up_t a_big0 = nsimd::upcvt(a._raw, val_t(), up_t()).v0;
-  simd_up_t a_big1 = nsimd::upcvt(a._raw, val_t(), up_t()).v1;
-  simd_up_t b_big0 = nsimd::upcvt(b._raw, val_t(), up_t()).v0;
-  simd_up_t b_big1 = nsimd::upcvt(b._raw, val_t(), up_t()).v1;
+  return fpsimd_uc<_lf,_rt>(
+        nsimd::shr(nsimd::mul(a._v1, b._v1, up_t()), shift, up_t())
+      , nsimd::shr(nsimd::mul(a._v2, b._v2, up_t()), shift, up_t())
+      );
+}
 
-  a_big0 = nsimd::mul(a_big0, b_big0, up_t());
-  a_big0 = nsimd::shr(a_big0, shift, up_t());
-  a_big1 = nsimd::mul(a_big1, b_big1, up_t());
-  a_big1 = nsimd::shr(a_big1, shift, up_t());
+template <uint8_t _lf, uint8_t _rt>
+NSIMD_INLINE fpsimd_t<_lf, _rt> simd_mul(const fpsimd_t<_lf, _rt> &a,
+                                         const fpsimd_t<_lf, _rt> &b) {
+  fpsimd_uc<_lf,_rt> aa = simd_decompress( a );
+  fpsimd_uc<_lf,_rt> bb = simd_decompress( b );
 
-  fpsimd_t<_lf, _rt> res;
-  res._raw = nsimd::downcvt(a_big0, a_big1, up_t(), val_t());
+  aa = simd_mul_uc( aa , bb );
 
+  fpsimd_t<_lf,_rt> res = simd_compress( aa );
   return res;
 }
 
@@ -60,6 +62,12 @@ template <uint8_t _lf, uint8_t _rt>
 NSIMD_INLINE fpsimd_t<_lf, _rt> operator*(const fpsimd_t<_lf, _rt> &a,
                                           const fpsimd_t<_lf, _rt> &b) {
   return simd_mul(a, b);
+}
+
+template <uint8_t _lf, uint8_t _rt>
+NSIMD_INLINE fpsimd_uc<_lf, _rt> operator*(const fpsimd_uc<_lf, _rt> &a,
+                                           const fpsimd_uc<_lf, _rt> &b) {
+  return simd_mul_uc(a, b);
 }
 
 } // namespace fixed_point
