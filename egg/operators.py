4933cc53dfd669b8ee2f6bce6de183a4f050eff9
vianney <vianney.stricher@agenium.com>
2019-12-19 16:23:18 +0100

    Subs(Operator) - python operators.py - fix typo in full_name + desc: substraction --> subtraction

--- egg/operators.py
+++ egg/operators.py
@@ -1125,12 +1125,12 @@ class Adds(Operator):
     bench_auto_against_mipp = True
 
 class Subs(Operator):
-    full_name = 'substraction using saturation'
+    full_name = 'subtraction using saturation'
     signature = 'v subs v v'
     types = common.types
     domain = Domain('RxR')
     categories = [DocBasicArithmetic]
-    desc = 'Returns the saturated substraction of the two vectors given as arguments'
+    desc = 'Returns the saturated subtraction of the two vectors given as arguments'
     bench_auto_against_std = True
     bench_auto_against_mipp = True
 
