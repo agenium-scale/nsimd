41bb6ee54c32efabacd335377c239f25d6805d4e
gquintin <guillaume.quintin@numscale.com>
2020-05-08 16:46:40 +0200

    Forgot file

---
+++ tests/modules/spmd/common.hpp
@@ -0,0 +1,171 @@
+/*
+
+Copyright (c) 2019 Agenium Scale
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#ifndef NSIMD_MODULES_SPMD_COMMON_HPP
+#define NSIMD_MODULES_SPMD_COMMON_HPP
+
+#include <nsimd/nsimd.h>
+#include <nsimd/scalar_utilities.h>
+#include <iostream>
+#include <cstring>
+#include <cerrno>
+#include <cstdlib>
+
+// ----------------------------------------------------------------------------
+// CUDA
+
+#if defined(NSIMD_CUDA)
+
+// perform reduction on blocks first, note that this could be optimized
+// but to check correctness we don't need it now
+template <typename T>
+__global__ void device_cmp_blocks(T *src1, T *src2, int n) {
+  extern __shared__ char buf_[]; // size of a block
+  T *buf = (T*)buf_;
+  int tid = threadIdx.x;
+  int i = tid + blockIdx.x * blockDim.x;
+  if (i < n) {
+    printf("DEBUG: %d: %f vs. %f\n", i, (double)src1[i], (double)src2[i]);
+    buf[tid] = T(nsimd::gpu_eq(src1[i], src2[i]) ? 1 : 0);
+  }
+  __syncthreads();
+  for (int s = blockDim.x / 2; s != 0; s /= 2) {
+    if (tid < s && i < n) {
+      buf[tid] = nsimd::gpu_mul(buf[tid], buf[tid + s]);
+      __syncthreads();
+    }
+  }
+  if (tid == 0) {
+    src1[i] = buf[0];
+  }
+}
+
+template <typename T>
+__global__ void device_cmp_array(int *dst, T *src1, int n) {
+  // reduction on the whole vector
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  T buf = T(1);
+  for (int i = 0; i < n; i += blockDim.x) {
+    buf = nsimd::gpu_mul(buf, src1[i]);
+  }
+  if (i == 0) {
+    dst[0] = int(buf);
+  }
+}
+
+template <typename T> bool cmp(T *src1, T *src2, unsigned int n) {
+  int host_ret;
+  int *device_ret;
+  if (cuda_do(cudaMalloc((void **)&device_ret, sizeof(int)))) {
+    return false;
+  }
+  device_cmp_blocks<<<(n + 127) / 128, 128, 128 * sizeof(T)>>>(src1, src2,
+                                                               int(n));
+  device_cmp_array<<<(n + 127) / 128, 128>>>(device_ret, src1, int(n));
+  if (cuda_do(cudaMemcpy((void *)&host_ret, (void *)device_ret, sizeof(int),
+                 cudaMemcpyDeviceToHost))) {
+    host_ret = 0;
+  }
+  cudaFree((void *)device_ret);
+  return bool(host_ret);
+}
+
+template <typename T> void del(T *ptr) { cudaFree(ptr); }
+
+#elif defined(NSIMD_ROCM)
+
+// ----------------------------------------------------------------------------
+// HIP
+
+// perform reduction on blocks first, note that this could be optimized
+// but to check correctness we don't need it now
+template <typename T>
+__global__ void device_cmp_blocks(T *src1, T *src2, unsigned int n) {
+  extern __shared__ char buf_[]; // size of a block
+  T *buf = (T*)buf_;
+  unsigned int tid = hipThreadIdx_x;
+  unsigned int i = tid + hipBlockIdx_x * hipBlockDim_x;
+  if (i < n) {
+    //printf("DEBUG: %d: %f vs. %f\n", i, (double)src1[i], (double)src2[i]);
+    buf[tid] = T(nsimd::gpu_eq(src1[i], src2[i]) ? 1 : 0);
+  }
+  __syncthreads();
+  for (unsigned int s = hipBlockDim_x / 2; s != 0; s /= 2) {
+    if (tid < s && i < n) {
+      buf[tid] = nsimd::gpu_mul(buf[tid], buf[tid + s]);
+      __syncthreads();
+    }
+  }
+  if (tid == 0) {
+    src1[i] = buf[0];
+  }
+}
+
+template <typename T>
+__global__ void device_cmp_array(int *dst, T *src1, unsigned int n) {
+  // reduction on the whole vector
+  unsigned int i = hipThreadIdx_x + hipBlockIdx_x * hipBlockDim_x;
+  T buf = T(1);
+  for (unsigned int i = 0; i < n; i += blockDim.x) {
+    buf = nsimd::gpu_mul(buf, src1[i]);
+  }
+  if (i == 0) {
+    dst[0] = int(buf);
+  }
+}
+
+template <typename T> bool cmp(T *src1, T *src2, unsigned int n) {
+  int host_ret;
+  int *device_ret;
+  if (hip_do(hipMalloc((void **)&device_ret, sizeof(int)))) {
+    return false;
+  }
+  hipLaunchKernelGGL(device_cmp_blocks, (n + 127) / 128, 128, 128 * sizeof(T),
+                     0, src1, src2, n);
+  hipLaunchKernelGGL(device_cmp_array, (n + 127) / 128, 128, 0, 0, device_ret,
+                     src1, n);
+  if (hip_do(hipMemcpy((void *)&host_ret, (void *)device_ret, sizeof(int),
+                 hipMemcpyDeviceToHost))) {
+    host_ret = 0;
+  }
+  hipFree((void *)device_ret);
+  return bool(host_ret);
+}
+
+template <typename T> void del(T *ptr) { hipFree(ptr); }
+
+#else
+
+// ----------------------------------------------------------------------------
+// SIMD
+
+template <typename T> bool cmp(T *src1, T *src2, unsigned int n) {
+  return memcmp(src1, src2, n * sizeof(T)) == 0;
+}
+
+#endif
+
+// ----------------------------------------------------------------------------
+
+#endif
