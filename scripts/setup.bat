cf2f94fb3aee1b582080efa3fdf3df2fe2621832
gquintin <guillaume.quintin@numscale.com>
2020-12-16 03:42:49 +0100

    Fix Windows script for building

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -29,11 +29,14 @@ REM ###########################################################################
 REM Init
 
 set NSTOOLS_DIR="%CD%\..\nstools"
-set NSTOOLS_URL="git@github.com:agenium-scale/nstools.git"
-set NSTOOLS_URL2="https://github.com/agenium-scale/nsimd.git"
 
 REM ###########################################################################
 REM Pull nstools
+
+git remote get-url origin >_tmp-nsimd-url.txt
+set /P NSIMD_URL=<_tmp-nsimd-url.txt
+set NSTOOLS_URL=%NSIMD_URL:nstools=nsimd%
+del /F /Q _tmp-nsimd-url.txt
 
 if exist "%NSTOOLS_DIR%\README.md" (
   pushd %NSTOOLS_DIR%
@@ -41,7 +44,7 @@ if exist "%NSTOOLS_DIR%\README.md" (
   popd
 ) else (
   pushd ".."
-  git clone %NSTOOLS_URL% || git clone %NSTOOLS_URL2%
+  git clone %NSTOOLS_URL% nstools
   popd
 )
 
@@ -58,8 +61,8 @@ REM Build nsconfig (if not already built)
 pushd %NSTOOLS_DIR%\nsconfig
 nmake /F Makefile.win nsconfig.exe
 nmake /F Makefile.win nstest.exe
-copy "nsconfig.exe" %NSTOOLS_DIR%\bin
-copy "nstest.exe" %NSTOOLS_DIR%\bin
+copy /Y "nsconfig.exe" %NSTOOLS_DIR%\bin
+copy /Y "nstest.exe" %NSTOOLS_DIR%\bin
 popd
 
 popd
