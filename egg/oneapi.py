42dd2f9b9e74091e67933b25f86e8e34dcb6cf25
vstricher <vianney.stricher@agenium.com>
2021-03-10 19:04:35 +0100

    egg/oneapi.py - implementation (in progress)

--- egg/oneapi.py
+++ egg/oneapi.py
@@ -30,34 +30,40 @@ fmtspec = dict()
 # -----------------------------------------------------------------------------
 
 def get_impl_f16(operator, totyp, typ):
-    # TODO: what's the difference between rec and rec8,rec11?
+    # sycl reference half type API:
+    # https://mmha.github.io/syclreference/libraries/types/half/
 
-    # Case 1: operators do not support sycl half types
-    # do not use nsimd casts. Use nsimd_scalar_functions
+    # Case 1: no sycl function available for half type
+    # sycl function available for f32
+    # however do not use nsimd casts f32 to f16 with rounding functions
+    # use nsimd_scalar_[operator]_f16
 
-    op_f16_use_nsimd_scalar = ['round_to_even', 'ceil', 'floor',
-                              'trunc','rec8','rec11','rsqrt8',
-                              'rsqrt11']
+    no_sycl_avail_f16_rounding_use_nsimd_scalar = \
+    ['round_to_even', 'ceil', 'floor', 'trunc']
 
-    # Case 2: operators do not support sycl half types
-    # use nsimd casts, then sycl provided functions
+    # Case 2: no sycl function available for half type
+    # sycl function available for f32
+    # use nsimd casts f32-->f16 + sycl function + f16-->f32
 
-    op_f16_cvrt_then_use_sycl_f32_op = ['fma', 'fms', 'fnma', 'fnms',
-                                       'fmin', 'fmax']
+    no_sycl_avail_f16_cast_use_sycl_f32 = \
+    ['fma', 'fms', 'fnma', 'fnms', 'min', 'max']
 
     # Case 3: sycl provides functions supporting half type
 
-    op_f16_half_type_sycl = ['rec']
+    sycl_avail_f16 = ['rec8', 'rec11', 'rec', 'rsqrt8', 'rsqrt11', 'rsqrt']
+
+    # Case 4: no sycl function available for any type
+    # use nsimd_scalar_[operator]_f16
 
     # Dispatch
 
     # Case 1
-    if operator.name in op_f16_use_nsimd_scalar:
+    if operator.name in no_sycl_avail_f16_rounding_use_nsimd_scalar:
       code = 'return nsimd_scalar_{op}_f16({in0});'.\
-      format(op=operator.name,**fmtspec)
+        format(op=operator.name,**fmtspec)
 
     # Case 2
-    elif operator.name in op_f16_cvrt_then_use_sycl_f32_op:
+    elif operator.name in no_sycl_avail_f16_cast_use_sycl_f32:
       if operator.name in ['fma', 'fms', 'fnma', 'fnms']:
         neg = '-' if operator.name in ['fnma, fnms'] else ''
         op = '-' if operator.name in ['fnms, fms'] else ''
@@ -68,8 +74,9 @@ def get_impl_f16(operator, totyp, typ):
            f32 res = sycl::fma({neg}{in0}, {in1}, {op}{in2});
            return nsimd_f32_to_f16(res);
         '''.format(neg=neg, op=op, **fmtspec)
-      if operator.name in ['min','max']:
+      elif operator.name in ['min','max']:
         op = 'fmin' if operator.name == 'min' else 'fmax'
+        code = \
         '''f32 in0 = nsimd_f16_to_f32({in0});
 	   f32 in1 = nsimd_f16_to_f32({in1});
            f32 res = sycl::{op}({in0}, {in1});
@@ -77,37 +84,39 @@ def get_impl_f16(operator, totyp, typ):
         '''.format(op=op, **fmtspec)
 
     # Case 3
-    elif operator.name in op_f16_half_type_sycl:
-      if operator.name == 'rec':
+    elif operator.name in sycl_avail_f16:
+      if operator.name in ['rec8', 'rec11', 'rec']:
         code = 'return sycl::recip({in0});'.format(**fmtspec)
+      elif operator.name in ['rsqrt8', 'rsqrt11', 'rsqrt']:
+        code = 'return sycl::rsqrt({in0});'.format(**fmtspec)
 
-    # ------------ below placeholder from CUDA (to update to oneAPI)-----------
-
-    elif operator.name in ['adds', 'subs']:
-        arch53_code = 'return __h{op}({in0}, {in1});'. \
-                      format(op=operator.name[:-1], **fmtspec)
+    # Case 4: no sycl function available for any type
+    # use nsimd_scalar_[operator]
     else:
-        args = ', '.join(['{{in{}}}'.format(i).format(**fmtspec) \
+      args = ', '.join(['{{in{}}}'.format(i).format(**fmtspec) \
                           for i in range(len(operator.params[1:]))])
-        # Some f16 functions are not prefixed by `__`
-        not_prefixed = ['ceil', 'floor', 'trunc', 'sqrt']
-        if operator.name in not_prefixed:
-            arch53_code = 'return h{}({});'.format(operator.name, args)
-        else:
-            arch53_code = 'return __h{}({});'.format(operator.name, args)
-    args = ', '.join(['__half2float({{in{}}})'.format(i).format(**fmtspec) \
-                      for i in range(len(operator.params[1:]))])
-    if operator.params[0] == 'l':
-        emul = 'return gpu_{}({});'.format(operator.name, args)
-    else:
-        emul = 'return __float2half(gpu_{}({}));'.format(operator.name, args)
-    return '''#if __CUDA_ARCH__ >= 530
-                {arch53_code}
-              #else
-                {emul}
-              #endif'''.format(arch53_code=arch53_code, emul=emul)
 
-# ------------------------ below draft to be updated ------------------
+      code = 'return nsimd_scalar_{op}_f16({args});'.\
+        format(op=operator.name,args)
+
+    return code
+
+
+# -----------------------------------------------------------------------------
+
+def reinterpret(totyp, typ):
+    if typ == totyp:
+        return 'return {in0};'.format(**fmtspec)
+    elif ((typ in common.ftypes and totyp in common.iutypes) or \
+    (typ in common.iutypes and totyp in common.ftypes)):
+    return 'return nsimd_scalar_reinterpret_{totyp}_{typ}({in0});'. \
+           format(totyp=totyp, typ=typ, **fmtspec)
+    else:
+        return '''{totyp} ret;
+                  memcpy((void *)&ret, (void *)&{in0}, sizeof({in0}));
+                  return ret;'''.format(**fmtspec)
+
+# -----------------------------------------------------------------------------
 
 def get_impl(operator, totyp, typ):
   global fmtspec
@@ -121,46 +130,99 @@ def get_impl(operator, totyp, typ):
   'typnbits': typ[1:]
   }
 
-  # Duplicated code with cuda (bool_operators, infix_operators)
-  # TODO: factorize
-
   # bool first, no special treatment for f16's
-  bool_operators = {
-  'andl': 'return {in0} && {in1};',
-  'orl': 'return {in0} || {in1};',
-  'xorl': 'return {in0} ^ {in1};',
-  'andnotl': 'return {in0} && (!{in1});',
-  'notl': 'return !{in0};',
-  }
+  bool_operators = [ 'andl', 'orl', 'xorl', 'andnotl', 'notl' ]
   if operator.name in bool_operators:
-    return bool_operators[operator.name].format(**fmtspec)
+    return 'return nsimd_scalar_{op}({in0},{in1})'.\
+            format(op=operator.name,**fmtspec)
 
-  # infix operators that needs type punning, no special treatment for f16's
-  def pun_code(code, arity, typ):
-      if typ in common.utypes:
-          return 'return ' + code.format(**fmtspec) + ';'
-      utyp = common.bitfield_type[typ]
-      to_utyp = '\n'.join(
-                ['''{utyp} buf{i};
-                    memcpy(&buf{i}, &{{in{i}}}, sizeof({{in{i}}}));'''. \
-                    format(i=i, utyp=utyp).format(**fmtspec) \
-                    for i in range(arity)])
-      return '''{to_utyp}
-                {utyp} tmp = {code};
-                {typ} ret;
-                memcpy(&ret, &tmp, sizeof(tmp));
-                return ret;'''.format(to_utyp=to_utyp, utyp=utyp, typ=typ,
-                                      code=code.format(in0='buf0',
-                                                       in1='buf1'))
-  pun_operators = {
-      'orb': lambda: pun_code('{in0} | {in1}', 2, typ),
-      'andb': lambda: pun_code('{in0} & {in1}', 2, typ),
-      'andnotb': lambda: pun_code('{in0} & (~{in1})', 2, typ),
-      'notb': lambda: pun_code('~{in0}', 1, typ),
-      'xorb': lambda: pun_code('{in0} ^ {in1}', 2, typ),
+  # infix operators no special treatment for f16's
+  infix_operators = [ 'orb', 'andb', 'andnotb', 'notb', 'xorb' ]
+  if operator.name in infix_operators:
+    return 'return nsimd_scalar_{op}_{typ}({in0},{in1})'.\
+            format(op=operator.name,**fmtspec)
+
+  # reinterpret
+  if operator.name == 'reinterpret':
+    return reinterpret(totyp, typ)
+
+  # cvt
+  if operator.name == 'cvt':
+    return 'return nsimd_scalar_cvt_{totyp}_{typ}({in0});'.format(**fmtspec)
+
+  # to_mask
+  if operator.name == 'to_mask':
+    return 'nsimd_scalar_to_mask_{totyp}({in0});'.format(**fmtspec)
+
+  # to_logical
+  if operator.name == 'to_logical':
+    return 'nsimd_scalar_to_logical_{typ}({in0});'.format(**fmtspec)
+
+  # for all other operators, f16 has a special treatment
+  if typ == 'f16':
+      return get_impl_f16(operator, totyp, typ)
+
+  # infix operators - f32, f64
+  infix_operators_f32_f64 = {
+      'rec': 'return sycl::recip({in0});',
+      'rec8': 'return sycl::recip({in0});',
+      'rec11': 'return sycl::recip({in0});',
+      'lt': 'return sycl::isless({in0},{in1});',
+      'gt': 'return sycl::isgreater({in0},{in1});',
+      'le': 'return sycl::islessequal({in0},{in1});',
+      'ge': 'return sycl::isgreaterequal({in0},{in1});',
+      'ne': 'return sycl::isnotequal({in0},{in1});',
+      'eq': 'return sycl::isequal({in0},{in1});'
   }
-  if operator.name in pun_operators:
-      return pun_operators[operator.name]()
+
+  if typ in ['f32','f64'] and operator.name in infix_operators_f32_f64:
+    return infix_operators_f32_f64[operator.name].\
+     format(**fmtspec)
+
+  # infix operators - cmp - integer types
+  infix_cmp_op_t = [ 'lt', 'gt', 'le', 'ge', 'ne', 'eq' ]
+  if operator.name in infix_cmp_op_t:
+    return 'return nsimd_scalar_{op}_{typ}({in0},{in1});'.\
+      format(op=operator.name, **fmtspec)
+
+  # infix operators f32, f64 + integers
+  # TODO: should use nsimd_scalar instead?
+  # ref: see book, pages 480, 481, 482
+  infix_operators_t = {
+      'add': 'return std::plus<{typ}>()({in0}, {in1});',
+      'sub': 'return std::minus<{typ}>()({in0}, {in1});',
+      'mul': 'return std::multiplies<{typ}>()({in0}, {in1});',
+      'div': 'return std::divides<{typ}>()({in0}, {in1});',
+      'neg': 'return std::negate<{typ}>()({in0});'
+  }
+
+  if operator.name in infix_operators_t:
+    return infix_operators_t[operator.name].\
+      format(**fmtspec)
+
+  # shifts
+  shifts_op_ui_t = [ 'shl', 'shr', 'shra' ]
+  if operator.name in shifts_op_ui_t and typ in common.iutypes:
+    return 'return nsimd_scalar_shl_{typ}({in0}, {in1});'.\
+      format(**fmtspec)
+
+  # adds
+  if operator.name == 'adds':
+    if typ in common.ftypes:
+      return infix_operators_t['add'].\
+        format(**fmtspec)
+    else:
+      return 'return sycl::add_sat({in0},{in1});'.format(**fmtspec)
+
+  # subs
+  if operator.name == 'subs':
+    if typ in common.ftypes:
+      return infix_operators_t['sub'].\
+        format(**fmtspec)
+    else:
+      return 'return sycl::sub_sat({in0},{in1});'.format(**fmtspec)
+
+  # ---------------- below in progress -------------------
 
   if operator.name == 'trunc':
    return 'return sycl::trunc({in0});'.format(**fmtspec)
@@ -171,7 +233,4 @@ def get_impl(operator, totyp, typ):
 
   if operator.name in ['floor', 'ceil', 'sqrt']:
     return 'return sycl::{op}({in0});'.format(op=operator.name, **fmtspec)
-
-
-
 
